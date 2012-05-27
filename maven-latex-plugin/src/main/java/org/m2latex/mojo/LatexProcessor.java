55aa535e23e696c62f1386e4fc86c58df6933dfd
hschoefer <henry.schoefer@googlemail.com>
2012-05-27 19:42:26 +0200

    Implemented biblatex support by executing makeindex. Fixed problem at table of content build.

--- maven-latex-plugin/src/main/java/org/m2latex/mojo/LatexProcessor.java
+++ maven-latex-plugin/src/main/java/org/m2latex/mojo/LatexProcessor.java
@@ -24,128 +24,183 @@ import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.util.cli.CommandLineException;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
-public class LatexProcessor
-{
-    static final String PATTERN_NEED_ANOTHER_LATEX_RUN = "(Rerun (LaTeX|to get cross-references right)|There were undefined references|Package natbib Warning: Citation\\(s\\) may have changed)";
+public class LatexProcessor {
+	static final String PATTERN_NEED_ANOTHER_LATEX_RUN = "(Rerun (LaTeX|to get cross-references right)*|There were undefined references|Package natbib Warning: Citation\\(s\\) may have changed)";
 
-    private final Settings settings;
+	private final Settings settings;
 
-    private final Log log;
+	private final Log log;
 
-    private final CommandExecutor executor;
+	private final CommandExecutor executor;
 
-    private TexFileUtils fileUtils;
+	private TexFileUtils fileUtils;
 
-    public LatexProcessor( Settings settings, CommandExecutor executor, Log log, TexFileUtils fileUtils )
-    {
-        this.settings = settings;
-        this.executor = executor;
-        this.log = log;
-        this.fileUtils = fileUtils;
-    }
+	public LatexProcessor(Settings settings, CommandExecutor executor, Log log,
+			TexFileUtils fileUtils) {
+		this.settings = settings;
+		this.executor = executor;
+		this.log = log;
+		this.fileUtils = fileUtils;
+	}
 
-    public void processLatex( File texFile )
-            throws CommandLineException, MojoExecutionException
-    {
-        log.info( "Processing LaTeX file " + texFile );
+	public void processLatex(File texFile) throws CommandLineException,
+			MojoExecutionException {
+		log.info("Processing LaTeX file " + texFile);
 
-        runLatex( texFile );
-        if ( needBibtexRun( texFile ) )
-        {
-            runBibtex( texFile );
-        }
-        int retries = 0;
-        while ( retries < 5 && needAnotherLatexRun( texFile ) )
-        {
-            log.debug( "Latex must be rerun" );
-            runLatex( texFile );
-            retries++;
-        }
-    }
+		runLatex(texFile);
+		if (settings.isUseBiblatex()) {
+			runMakeindex(texFile);
+		}
 
-    public void processTex4ht( File texFile )
-            throws MojoExecutionException, CommandLineException
-    {
-        processLatex( texFile );
-        runTex4ht( texFile );
-    }
+		if (needBibtexRun(texFile)) {
+			runBibtex(texFile);
+		}
+		int retries = 0;
+		while (retries < 5 && needAnotherLatexRun(texFile)) {
+			log.debug("Latex must be rerun");
+			runLatex(texFile);
+			retries++;
+		}
+	}
 
-    private void runTex4ht( File texFile )
-            throws CommandLineException, MojoExecutionException
-    {
-        log.debug( "Running " + settings.getTex4htCommand() + " on file " + texFile.getName() );
-        File workingDir = texFile.getParentFile();
-        String[] args = buildHtlatexArguments( texFile );
-        executor.execute( workingDir, settings.getTexPath(), settings.getTex4htCommand(), args );
-    }
+	public void processTex4ht(File texFile) throws MojoExecutionException,
+			CommandLineException {
+		processLatex(texFile);
+		runTex4ht(texFile);
+	}
 
-    private String[] buildHtlatexArguments( File texFile )
-            throws MojoExecutionException
-    {
-        File tex4htOutdir = fileUtils.createTex4htOutputDir( settings.getTempDirectory() );
+	private void runTex4ht(File texFile) throws CommandLineException,
+			MojoExecutionException {
+		log.debug("Running " + settings.getTex4htCommand() + " on file "
+				+ texFile.getName());
+		File workingDir = texFile.getParentFile();
+		String[] args = buildHtlatexArguments(texFile);
+		executor.execute(workingDir, settings.getTexPath(),
+				settings.getTex4htCommand(), args);
+	}
 
-        final String argOutputDir = " -d" + tex4htOutdir.getAbsolutePath() + File.separatorChar;
-        String[] tex4htCommandArgs = settings.getTex4htCommandArgs();
+	private String[] buildHtlatexArguments(File texFile)
+			throws MojoExecutionException {
+		File tex4htOutdir = fileUtils.createTex4htOutputDir(settings
+				.getTempDirectory());
 
-        String htlatexOptions = getTex4htArgument( tex4htCommandArgs, 0 );
-        String tex4htOptions = getTex4htArgument( tex4htCommandArgs, 1 );
-        String t4htOptions = getTex4htArgument( tex4htCommandArgs, 2 ) + argOutputDir;
-        String latexOptions = getTex4htArgument( tex4htCommandArgs, 3 );
+		final String argOutputDir = " -d" + tex4htOutdir.getAbsolutePath()
+				+ File.separatorChar;
+		String[] tex4htCommandArgs = settings.getTex4htCommandArgs();
 
-        String[] args = new String[5];
-        args[0] = texFile.getName();
-        args[1] = htlatexOptions;
-        args[2] = tex4htOptions;
-        args[3] = t4htOptions;
-        args[4] = latexOptions;
+		String htlatexOptions = getTex4htArgument(tex4htCommandArgs, 0);
+		String tex4htOptions = getTex4htArgument(tex4htCommandArgs, 1);
+		String t4htOptions = getTex4htArgument(tex4htCommandArgs, 2)
+				+ argOutputDir;
+		String latexOptions = getTex4htArgument(tex4htCommandArgs, 3);
 
-        return args;
-    }
+		String[] args = new String[5];
+		args[0] = texFile.getName();
+		args[1] = htlatexOptions;
+		args[2] = tex4htOptions;
+		args[3] = t4htOptions;
+		args[4] = latexOptions;
 
-    private String getTex4htArgument( String[] args, int index )
-    {
-        boolean returnEmptyArg = args == null || args.length < index + 1 || StringUtils.isEmpty( args[index] );
-        return returnEmptyArg ? "" : args[index];
-    }
+		return args;
+	}
 
-    private boolean needAnotherLatexRun( File texFile )
-            throws MojoExecutionException
-    {
-        String reRunPattern = PATTERN_NEED_ANOTHER_LATEX_RUN;
-        boolean needRun = fileUtils.matchInCorrespondingLogFile( texFile, reRunPattern );
-        log.debug( "Another Latex run? " + needRun );
-        return needRun;
-    }
+	private String getTex4htArgument(String[] args, int index) {
+		boolean returnEmptyArg = args == null || args.length < index + 1
+				|| StringUtils.isEmpty(args[index]);
+		return returnEmptyArg ? "" : args[index];
+	}
 
-    private boolean needBibtexRun( File texFile )
-            throws MojoExecutionException
-    {
-        String namePrefixTexFile = fileUtils.getFileNameWithoutSuffix( texFile );
-        String pattern = "No file " + namePrefixTexFile + ".bbl";
-        return fileUtils.matchInCorrespondingLogFile( texFile, pattern );
-    }
+	private boolean needAnotherLatexRun(File texFile)
+			throws MojoExecutionException {
+		String reRunPattern = PATTERN_NEED_ANOTHER_LATEX_RUN;
+		boolean needRun = fileUtils.matchInCorrespondingLogFile(texFile,
+				reRunPattern);
+		log.debug("Another Latex run? " + needRun);
+		return needRun;
+	}
 
-    private void runBibtex( File texFile )
-            throws CommandLineException
-    {
-        log.debug( "Running BibTeX on file " + texFile.getName() );
-        File workingDir = texFile.getParentFile();
+	private boolean needBibtexRun(File texFile) throws MojoExecutionException {
+		String namePrefixTexFile = fileUtils.getFileNameWithoutSuffix(texFile);
+		String pattern = "No file " + namePrefixTexFile + ".bbl";
+		return fileUtils.matchInCorrespondingLogFile(texFile, pattern);
+	}
 
-        String[] args = new String[]{fileUtils.getCorrespondingAuxFile( texFile ).getPath()};
-        executor.execute( workingDir, settings.getTexPath(), settings.getBibtexCommand(), args );
-    }
+	/**
+	 * Runs the makeindex binary of tex implementation.
+	 * 
+	 * @param texFile
+	 *            - Tex file which will be parsed by makeindex.
+	 * @throws CommandLineException
+	 */
+	private void runMakeindex(File texFile) throws CommandLineException {
+		log.debug("Running makeindex on file " + texFile.getName());
+		File workingDir = texFile.getParentFile();
 
-    private void runLatex( File texFile )
-            throws CommandLineException
-    {
-        log.debug( "Running " + settings.getTexCommand() + " on file " + texFile.getName() );
-        File workingDir = texFile.getParentFile();
+		List parameter = new LinkedList();
 
-        String[] texCommandArgs = settings.getTexCommandArgs();
-        String[] args = new String[texCommandArgs.length + 1];
-        System.arraycopy( texCommandArgs, 0, args, 0, texCommandArgs.length );
-        args[texCommandArgs.length] = texFile.getName();
-        executor.execute( workingDir, settings.getTexPath(), settings.getTexCommand(), args );
-    }
+		String filePath = null;
+
+		File istFile = fileUtils.getCorrespondingIstFile(texFile);
+		if (istFile.exists()) {
+			filePath = istFile.getPath();
+			parameter.add(istFile.getName());
+			log.debug("ist file parameter: " + istFile.getName());
+		}
+
+		File glgFile = fileUtils.getCorrespondingGlgFile(texFile);
+		if (glgFile.exists()) {
+			filePath = glgFile.getPath();
+			parameter.add("-t");
+			parameter.add(filePath);
+			log.debug("glg file parameter: " + filePath);
+		}
+
+		File glsFile = fileUtils.getCorrespondingGlsFile(texFile);
+		if (glsFile.exists()) {
+			filePath = glsFile.getPath();
+			parameter.add("-o");
+			parameter.add(filePath);
+			log.debug("gls file parameter: " + filePath);
+		}
+
+		File gloFile = fileUtils.getCorrespondingGloFile(texFile);
+		if (gloFile.exists()) {
+			parameter.add(gloFile.getName());
+			log.debug("glo file parameter: " + gloFile.getName());
+		}
+
+		String[] args = new String[parameter.size()];
+		for (int i = 0; i < parameter.size(); i++) {
+			args[i] = (String) parameter.get(i);
+		}
+
+		executor.execute(workingDir, settings.getTexPath(),
+				settings.getMakeindexCommand(), args);
+	}
+
+	private void runBibtex(File texFile) throws CommandLineException {
+		log.debug("Running BibTeX on file " + texFile.getName());
+		File workingDir = texFile.getParentFile();
+
+		String[] args = new String[] { fileUtils.getCorrespondingAuxFile(
+				texFile).getName() };
+		executor.execute(workingDir, settings.getTexPath(),
+				settings.getBibtexCommand(), args);
+	}
+
+	private void runLatex(File texFile) throws CommandLineException {
+		log.debug("Running " + settings.getTexCommand() + " on file "
+				+ texFile.getName());
+		File workingDir = texFile.getParentFile();
+
+		String[] texCommandArgs = settings.getTexCommandArgs();
+		String[] args = new String[texCommandArgs.length + 1];
+		System.arraycopy(texCommandArgs, 0, args, 0, texCommandArgs.length);
+		args[texCommandArgs.length] = texFile.getName();
+		executor.execute(workingDir, settings.getTexPath(),
+				settings.getTexCommand(), args);
+	}
 }
--- maven-latex-plugin/src/main/java/org/m2latex/mojo/Settings.java
+++ maven-latex-plugin/src/main/java/org/m2latex/mojo/Settings.java
@@ -23,260 +23,260 @@ import org.apache.commons.lang.StringUtils;
 import java.io.File;
 import java.util.Arrays;
 
-public class Settings
-{
-    /**
-     * @parameter
-     * @readonly
-     */
-    private File baseDirectory;
+public class Settings {
+	/**
+	 * @parameter
+	 * @readonly
+	 */
+	private File baseDirectory;
 
-    /**
-     * @parameter
-     * @readonly
-     */
-    private File targetSiteDirectory;
+	/**
+	 * @parameter
+	 * @readonly
+	 */
+	private File targetSiteDirectory;
 
-    /**
-     * @parameter
-     * @readonly
-     */
-    private File targetDirectory;
+	/**
+	 * @parameter
+	 * @readonly
+	 */
+	private File targetDirectory;
 
-    /**
-     * @parameter
-     * @readonly
-     */
-    private String tempDirectory = null;
+	/**
+	 * @parameter
+	 * @readonly
+	 */
+	private String tempDirectory = null;
 
-    /**
-     * @parameter
-     */
-    private File texDirectory = null;
+	/**
+	 * @parameter
+	 */
+	private File texDirectory = null;
 
-    /**
-     * @parameter
-     */
-    private String outputDirectory = null;
+	/**
+	 * @parameter
+	 */
+	private String outputDirectory = null;
 
-    /**
-     * @parameter
-     */
-    private File texPath = null;
+	/**
+	 * @parameter
+	 */
+	private File texPath = null;
 
-    /**
-     * @parameter
-     */
-    private String texCommand = "pdflatex";
+	/**
+	 * @parameter
+	 */
+	private String texCommand = "pdflatex";
 
-    /**
-     * @parameter
-     */
-    private String tex4htCommand = "htlatex";
+	/**
+	 * @parameter
+	 */
+	private String tex4htCommand = "htlatex";
 
-    /**
-     * @parameter
-     */
-    private String bibtexCommand = "bibtex";
+	/**
+	 * @parameter
+	 */
+	private String bibtexCommand = "bibtex";
 
-    /**
-     * @parameter
-     */
-    private boolean cleanUp = true;
+	/**
+	 * @parameter
+	 */
+	private boolean useBiblatex = false;
 
-    /**
-     * @parameter
-     */
-    private String[] texCommandArgs = new String[]{"-interaction=nonstopmode", "--src-specials"};
+	/**
+	 * @parameter
+	 */
+	private String makeindexCommand = "makeindex";
 
-    /**
-     * TODO move to different fields; take latex args from texCommandArgs
-     *
-     * @parameter
-     */
-    private String[] tex4htCommandArgs = new String[]{"html,2", "", "", "-interaction=nonstopmode --src-specials"};
+	/**
+	 * @parameter
+	 */
+	private boolean cleanUp = true;
 
-    private File outputDirectoryFile = null;
+	/**
+	 * @parameter
+	 */
+	private String[] texCommandArgs = new String[] {
+			"-interaction=nonstopmode", "--src-specials" };
 
-    private File tempDirectoryFile = null;
+	/**
+	 * TODO move to different fields; take latex args from texCommandArgs
+	 * 
+	 * @parameter
+	 */
+	private String[] tex4htCommandArgs = new String[] { "html,2", "", "",
+			"-interaction=nonstopmode --src-specials" };
 
-    public File getBaseDirectory()
-    {
-        return baseDirectory;
-    }
+	private File outputDirectoryFile = null;
 
-    public String getBibtexCommand()
-    {
-        return bibtexCommand;
-    }
+	private File tempDirectoryFile = null;
 
-    public File getOutputDirectory()
-    {
-        if ( outputDirectoryFile == null )
-        {
-            if ( StringUtils.isEmpty( outputDirectory ) )
-            {
-                outputDirectoryFile = targetSiteDirectory;
-            }
-            else
-            {
-                outputDirectoryFile = new File( targetSiteDirectory, outputDirectory );
-            }
-        }
-        return outputDirectoryFile;
-    }
+	public File getBaseDirectory() {
+		return baseDirectory;
+	}
 
-    public File getTargetDirectory()
-    {
-        return targetDirectory;
-    }
+	public String getBibtexCommand() {
+		return bibtexCommand;
+	}
 
-    public File getTargetSiteDirectory()
-    {
-        return targetSiteDirectory;
-    }
+	public boolean isUseBiblatex() {
+		return useBiblatex;
+	}
 
-    public File getTempDirectory()
-    {
-        if (tempDirectoryFile == null)
-        {
-            String dirName = StringUtils.isBlank( tempDirectory ) ? "m2latex" : tempDirectory;
-            tempDirectoryFile = new File( targetDirectory, dirName );
-        }
-        return tempDirectoryFile;
-    }
+	public String getMakeindexCommand() {
+		return makeindexCommand;
+	}
 
-    public String getTex4htCommand()
-    {
-        return tex4htCommand;
-    }
+	public File getOutputDirectory() {
+		if (outputDirectoryFile == null) {
+			if (StringUtils.isEmpty(outputDirectory)) {
+				outputDirectoryFile = targetSiteDirectory;
+			} else {
+				outputDirectoryFile = new File(targetSiteDirectory,
+						outputDirectory);
+			}
+		}
+		return outputDirectoryFile;
+	}
 
-    public String[] getTex4htCommandArgs()
-    {
-        return tex4htCommandArgs;
-    }
+	public File getTargetDirectory() {
+		return targetDirectory;
+	}
 
-    public String getTexCommand()
-    {
-        return texCommand;
-    }
+	public File getTargetSiteDirectory() {
+		return targetSiteDirectory;
+	}
 
-    public String[] getTexCommandArgs()
-    {
-        return texCommandArgs;
-    }
+	public File getTempDirectory() {
+		if (tempDirectoryFile == null) {
+			String dirName = StringUtils.isBlank(tempDirectory) ? "m2latex"
+					: tempDirectory;
+			tempDirectoryFile = new File(targetDirectory, dirName);
+		}
+		return tempDirectoryFile;
+	}
 
-    public File getTexDirectory()
-    {
-        if ( texDirectory == null )
-        {
-            texDirectory = new File( new File( new File( baseDirectory, "src" ), "site" ), "tex" );
-        }
-        return texDirectory;
-    }
+	public String getTex4htCommand() {
+		return tex4htCommand;
+	}
 
-    public File getTexPath()
-    {
-        return texPath;
-    }
+	public String[] getTex4htCommandArgs() {
+		return tex4htCommandArgs;
+	}
 
-    public boolean isCleanUp()
-    {
-        return cleanUp;
-    }
+	public String getTexCommand() {
+		return texCommand;
+	}
 
-    public Settings setBaseDirectory( File baseDirectory )
-    {
-        this.baseDirectory = baseDirectory;
-        return this;
-    }
+	public String[] getTexCommandArgs() {
+		return texCommandArgs;
+	}
 
-    public Settings setBibtexCommand( String bibtexCommand )
-    {
-        this.bibtexCommand = bibtexCommand;
-        return this;
-    }
+	public File getTexDirectory() {
+		if (texDirectory == null) {
+			texDirectory = new File(new File(new File(baseDirectory, "src"),
+					"site"), "tex");
+		}
+		return texDirectory;
+	}
 
-    public Settings setCleanUp( boolean cleanUp )
-    {
-        this.cleanUp = cleanUp;
-        return this;
-    }
+	public File getTexPath() {
+		return texPath;
+	}
 
-    public Settings setOutputDirectory( String outputDirectory )
-    {
-        this.outputDirectory = outputDirectory;
-        return this;
-    }
+	public boolean isCleanUp() {
+		return cleanUp;
+	}
 
-    public Settings setTargetDirectory( File targetDirectory )
-    {
-        this.targetDirectory = targetDirectory;
-        return this;
-    }
+	public Settings setBaseDirectory(File baseDirectory) {
+		this.baseDirectory = baseDirectory;
+		return this;
+	}
 
-    public Settings setTargetSiteDirectory( File targetSiteDirectory )
-    {
-        this.targetSiteDirectory = targetSiteDirectory;
-        return this;
-    }
+	public Settings setBibtexCommand(String bibtexCommand) {
+		this.bibtexCommand = bibtexCommand;
+		return this;
+	}
 
-    public Settings setTempDirectory( String tempDirectory )
-    {
-        this.tempDirectory = tempDirectory;
-        return this;
-    }
+	public Settings setUseBiblatex(boolean useBiblatex) {
+		this.useBiblatex = useBiblatex;
+		return this;
+	}
 
-    /**
-     * @parameter
-     */
-    public Settings setTex4htCommand( String tex4htCommand )
-    {
-        this.tex4htCommand = tex4htCommand;
-        return this;
-    }
+	public Settings setMakeindexCommand(String makeindexCommand) {
+		this.makeindexCommand = makeindexCommand;
+		return this;
+	}
 
-    public Settings setTex4htCommandArgs( String[] tex4htCommandArgs )
-    {
-        this.tex4htCommandArgs = tex4htCommandArgs;
-        return this;
-    }
+	public Settings setCleanUp(boolean cleanUp) {
+		this.cleanUp = cleanUp;
+		return this;
+	}
 
-    public Settings setTexCommand( String texCommand )
-    {
-        this.texCommand = texCommand;
-        return this;
-    }
+	public Settings setOutputDirectory(String outputDirectory) {
+		this.outputDirectory = outputDirectory;
+		return this;
+	}
 
-    public Settings setTexCommandArgs( String[] args )
-    {
-        this.texCommandArgs = args;
-        return this;
-    }
+	public Settings setTargetDirectory(File targetDirectory) {
+		this.targetDirectory = targetDirectory;
+		return this;
+	}
 
-    public Settings setTexDirectory( File texDirectory )
-    {
-        this.texDirectory = texDirectory;
-        return this;
-    }
+	public Settings setTargetSiteDirectory(File targetSiteDirectory) {
+		this.targetSiteDirectory = targetSiteDirectory;
+		return this;
+	}
 
-    public Settings setTexPath( File texPath )
-    {
-        this.texPath = texPath;
-        return this;
-    }
+	public Settings setTempDirectory(String tempDirectory) {
+		this.tempDirectory = tempDirectory;
+		return this;
+	}
 
-    public String toString()
-    {
-        StringBuffer sb = new StringBuffer( super.toString() );
-        sb.append( '[' ).append( "tempDirectory=" ).append( tempDirectory );
-        sb.append( ",texPath=" ).append( texPath );
-        sb.append( ",texCommand=" ).append( texCommand );
-        sb.append( ",bibtexCommand=" ).append( bibtexCommand );
-        sb.append( ",baseDirectory=" ).append( baseDirectory );
-        sb.append( ",targetSiteDirectory=" ).append( targetSiteDirectory );
-        sb.append( ",texDirectory=" ).append( texDirectory );
-        sb.append( ",texCommandArgs=" ).append( Arrays.asList( texCommandArgs ) ).append( ']' );
-        return sb.toString();
-    }
+	/**
+	 * @parameter
+	 */
+	public Settings setTex4htCommand(String tex4htCommand) {
+		this.tex4htCommand = tex4htCommand;
+		return this;
+	}
+
+	public Settings setTex4htCommandArgs(String[] tex4htCommandArgs) {
+		this.tex4htCommandArgs = tex4htCommandArgs;
+		return this;
+	}
+
+	public Settings setTexCommand(String texCommand) {
+		this.texCommand = texCommand;
+		return this;
+	}
+
+	public Settings setTexCommandArgs(String[] args) {
+		this.texCommandArgs = args;
+		return this;
+	}
+
+	public Settings setTexDirectory(File texDirectory) {
+		this.texDirectory = texDirectory;
+		return this;
+	}
+
+	public Settings setTexPath(File texPath) {
+		this.texPath = texPath;
+		return this;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer(super.toString());
+		sb.append('[').append("tempDirectory=").append(tempDirectory);
+		sb.append(",texPath=").append(texPath);
+		sb.append(",texCommand=").append(texCommand);
+		sb.append(",bibtexCommand=").append(bibtexCommand);
+		sb.append(",baseDirectory=").append(baseDirectory);
+		sb.append(",targetSiteDirectory=").append(targetSiteDirectory);
+		sb.append(",texDirectory=").append(texDirectory);
+		sb.append(",texCommandArgs=").append(Arrays.asList(texCommandArgs))
+				.append(']');
+		return sb.toString();
+	}
 }
