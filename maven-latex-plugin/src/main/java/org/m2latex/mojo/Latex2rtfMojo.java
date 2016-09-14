c365307aafdd71f323ee3883f34e77e65b69e687
Reissner <rei3ner@arcor.de>
2016-09-14 17:59:08 +0200

    @new 	added goal latex2rtf: added according mojo.

---
+++ maven-latex-plugin/src/main/java/org/m2latex/mojo/Latex2rtfMojo.java
@@ -0,0 +1,57 @@
+/*
+ * The akquinet maven-latex-plugin project
+ *
+ * Copyright (c) 2011 by akquinet tech@spree GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.m2latex.mojo;
+
+import java.io.File;
+import java.io.FileFilter;
+
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+/**
+ * Build rtf documents from LaTeX sources.
+ * 
+ * @goal latex2rtf
+ * @phase site
+ */
+//@Mojo( name = "latex2rtf")
+public class Latex2rtfMojo
+    extends AbstractLatexMojo
+{
+    // implements AbstractLatexMojo#processSource(File)
+    void processSource(File texFile) 
+	throws CommandLineException, MojoExecutionException {
+	this.latexProcessor.processLatex2rtf(texFile);
+    }
+
+    // implements AbstractLatexMojo#getOutputDir()
+    File getOutputDir() {
+	return this.settings.getTempDirectory();
+    }
+
+    // implements AbstractLatexMojo#getFileFilter(File)
+    FileFilter getFileFilter(File texFile) {
+	return this.fileUtils.getLatex2rtfOutputFileFilter(texFile);
+    }
+}
