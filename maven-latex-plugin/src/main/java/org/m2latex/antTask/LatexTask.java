0718071bbd7d350e645a0db0fe65b60878634aba
Reissner <rei3ner@arcor.de>
2016-09-28 16:40:29 +0200

    @new 	executes essentially PdfMojo (compiles but does not run.)

--- maven-latex-plugin/src/main/java/org/m2latex/antTask/LatexTask.java
+++ maven-latex-plugin/src/main/java/org/m2latex/antTask/LatexTask.java
@@ -2,7 +2,14 @@ package org.m2latex.antTask;
 
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.BuildException;
+
+//import org.apache.maven.plugin.Mojo;
+//import org.apache.maven.plugin.MojoFailureException;
+//import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.AbstractMojoExecutionException;
+
 import org.m2latex.mojo.Settings;
+import org.m2latex.mojo.PdfMojo;
 
 public class LatexTask extends Task {
 
@@ -12,7 +19,7 @@ public class LatexTask extends Task {
 	return this.settings = new Settings();
     }
 
-    public void execute() {
+    public void execute() throws BuildException {
         // use of the reference to Project-instance
         String message = getProject().getProperty("ant.project.name");
 
@@ -25,6 +32,12 @@ public class LatexTask extends Task {
             throw new BuildException("No settings found.");
         }
  	log("settings: \n" +  this.settings );
-    }
 
+	PdfMojo mojo = new PdfMojo();
+	try {
+	    mojo.execute();
+	} catch (AbstractMojoExecutionException e) {
+	    throw new BuildException(e.getMessage(), e.getCause());
+	}
+     }
  }
