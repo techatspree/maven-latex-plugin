0a71051c5a48a7ae2b49902d5ba0ab87aa3c86c7
Reissner <rei3ner@arcor.de>
2016-10-18 10:04:16 +0200

    @new 	support both backends of latex.

---
+++ maven-latex-plugin/src/main/java/org/m2latex/core/LatexDev.java
@@ -0,0 +1,19 @@
+package org.m2latex.core;
+
+/**
+ * Enumeration of the backends of latex. 
+ *
+ *
+ * Created: Tue Oct 18 10:06:30 2016
+ *
+ * @author <a href="mailto:rei3ner@arcor.de">Ernst Reissner</a>
+ * @version 1.0
+ */
+public enum LatexDev {
+
+    pdf {
+    },
+    dvips {
+    };
+
+}
