9585f7917c00326a83157e79b92166b3607d5a36
Reissner <rei3ner@arcor.de>
2016-10-26 08:38:45 +0200

    @chg 	renamed class

--- maven-latex-plugin/src/main/java/org/m2latex/antTask/LatexTask.java
+++ maven-latex-plugin/src/main/java/org/m2latex/antTask/LatexTask.java
@@ -14,7 +14,7 @@ import java.io.File;
 
 import java.util.SortedSet;
 
-public class LatexTask extends Task implements ParameterAdapter {
+public class LatexCfgTask extends Task implements ParameterAdapter {
 
     /**
      * Contains all parameters for executing this task. 
