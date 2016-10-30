202b245074a72bd902bd56b90b9254b5980ded03
Reissner <rei3ner@arcor.de>
2016-10-30 14:45:25 +0100

    @new 	clear-task.

---
+++ maven-latex-plugin/src/main/java/org/m2latex/antTask/LatexClrTask.java
@@ -0,0 +1,30 @@
+package org.m2latex.antTask;
+
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.BuildException;
+
+import org.m2latex.core.Settings;
+import org.m2latex.core.MyBuildException;
+import org.m2latex.core.LatexProcessor;
+import org.m2latex.core.ParameterAdapter;
+import org.m2latex.core.Target;
+
+import java.io.File;
+
+import java.util.SortedSet;
+
+public class LatexClrTask extends LatexCfgTask implements ParameterAdapter {
+
+    /**
+     * Invoked by ant executing the task. 
+     */
+    public void execute() throws BuildException {
+ 	initialize();
+	try {
+	    this.latexProcessor.clearAll();
+	} catch (MyBuildException e) {
+	    throw new BuildException(e.getMessage(), e.getCause());
+	}
+     }
+ }
