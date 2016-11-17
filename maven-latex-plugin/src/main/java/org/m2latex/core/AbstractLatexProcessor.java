4ddf824c24f4659fc46fb9cf754fef86ec682f4e
Reissner <rei3ner@arcor.de>
2016-11-17 11:28:00 +0100

    @new 	extracting preprocessing 	- added new common superclass AbstractLatexProcessor.

---
+++ maven-latex-plugin/src/main/java/org/m2latex/core/AbstractLatexProcessor.java
@@ -0,0 +1,22 @@
+package org.m2latex.core;
+
+/**
+ * Describe class AbstractLatexProcessor here.
+ *
+ *
+ * Created: Thu Nov 17 12:29:36 2016
+ *
+ * @author <a href="mailto:rei3ner@arcor.de">Ernst Reissner</a>
+ * @version 1.0
+ */
+abstract class AbstractLatexProcessor {
+
+    /**
+     * Creates a new <code>AbstractLatexProcessor</code> instance.
+     *
+     */
+    public AbstractLatexProcessor() {
+
+    }
+
+}
