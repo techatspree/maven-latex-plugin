a4095d3f958595168c3147e805bf74d127743f3d
Reissner <rei3ner@arcor.de>
2016-11-23 02:15:06 +0100

    @bugfix 	generated files to be copied to the target folder: 	- replace array of endings indicating copy by patterns. 	  e.g. renamed getOutputFileSuffixes-->getPatternOutputFileSuffixes 	  This gives stricter controll over the files to be copied 	- added method getPatternOutputFiles 	  which allows patterns not related with the root of the according latex main file. 	  (now symbols occur in html output).

--- maven-latex-plugin/src/main/java/org/m2latex/core/Target.java
+++ maven-latex-plugin/src/main/java/org/m2latex/core/Target.java
@@ -3,7 +3,9 @@ package org.m2latex.core;
 import java.io.File;
 
 /**
- * The enumeration of all supported targets. 
+ * The enumeration of all supported creational targets. 
+ * Additional targets are <code>clr</code> and <code>grp</code>. 
+ * The latter is not creational. 
  *
  *
  * Created: Fri Oct  7 13:32:21 2016
@@ -18,12 +20,12 @@ public enum Target {
      *
      */
     rtf() {
-	private final String[] RTF_OUTPUT_FILES = new String[] {".rtf"};
+	private final String RTF_OUTPUT_FILES = "\\.rtf";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2rtf(texFile);
 	}
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return RTF_OUTPUT_FILES;
 	}
 
@@ -32,14 +34,14 @@ public enum Target {
      * standalone. 
      */
     pdf() {
-	private final String[] LATEX_OUTPUT_FILES = new String[] {
-	    LatexProcessor.SUFFIX_PDF, ".dvi", ".ps"
-	};
+	private final String LATEX_OUTPUT_FILES = 
+	    "(" + LatexProcessor.SUFFIX_PDF + "|\\.dvi|\\.ps)";
+
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2pdf(texFile);
 	}
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return LATEX_OUTPUT_FILES;
 	}
     },
@@ -48,32 +50,44 @@ public enum Target {
      *
      */
     html() {
-	private final String[] HTML_OUTPUT_FILES = new String[] {
-	    "*.html", "*.xhtml", "*.htm", ".css", "*.png", "*.svg"
-	};
+	// FIXME: use class constants for suffixes 
+	// Documentation: 
+	// - xxx.(x)htm(l) is the main file. 
+	// - xxxlid.(x)htm(l) with digit(s) d 
+	//                    are lists: toc, lof, lot, indices, glossaries, 
+	//                    NOT the bibliography. 
+	// - xxxsed.(x)htm(l) with digit(s) d are sections. 
+	// - xxxsud.(x)htm(l) with digit(s) d are subsections or below. 
+	// - xxxd.(x)htm(l)   with digit(s) d are footnotes 
+	private final String HTML_OUTPUT_FILES = 
+	    "(((se|su|li)?\\d+)?\\.x?html?|" + 
+	    "\\.css|" + 
+	    ".\\d+\\.svg)";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2html(texFile);
 	}
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return HTML_OUTPUT_FILES;
+	}
+	public String getPatternOutputFiles() {
+	    return "(cmsy)\\d+(-c)?-\\d+c?\\.png";
 	}
 
     },
-     /**
+    /**
      * Based on {@link #pdf}
      *
      */
     odt() {
-	private final String[] OOFFICE_OUTPUT_FILES = new String[] {
-	    ".odt", ".fodt", ".uot", ".uot"
-	};
+	private final String OOFFICE_OUTPUT_FILES =
+	    "(.odt|.fodt|.uot|.uot)";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2odt(texFile);
 	}
 
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return OOFFICE_OUTPUT_FILES;
 	}
     },
@@ -82,14 +96,13 @@ public enum Target {
      *
      */
     docx() {
-	private final String[] MSWORD_OUTPUT_FILES = new String[] {
-	    ".doc", ".doc6", ".doc95", ".docx", ".docx7", ".rtf"
-	};
+	private final String MSWORD_OUTPUT_FILES = 
+	    "(\\.doc(|6|.95|.x|.x7)|\\.rtf)";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2docx(texFile);
 	}
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return MSWORD_OUTPUT_FILES;
 	}
 
@@ -98,12 +111,12 @@ public enum Target {
      * Based on {@link #pdf}
      */
     txt() {
-	private final String[] TXT_OUTPUT_FILES = new String[] {".txt"};
+	private final String TXT_OUTPUT_FILES = ".txt";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2txt(texFile);
 	}
-	public String[] getOutputFileSuffixes() {
+	public String getPatternOutputFileSuffixes() {
 	    return TXT_OUTPUT_FILES;
 	}
     };
@@ -124,6 +137,11 @@ public enum Target {
      * but if various html files are created, it is <code>*.html</code>, 
      * the asterisk representing a wildcard. 
      */
-    public abstract String[] getOutputFileSuffixes();
+    public abstract String getPatternOutputFileSuffixes();
+
+    public String getPatternOutputFiles() {
+	return "^$";
+    }
 
 }
+ 
