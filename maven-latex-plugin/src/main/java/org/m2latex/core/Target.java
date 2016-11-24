bb5f6efc59bb50e7d050708c267104ac253a1c38
Reissner <rei3ner@arcor.de>
2016-11-25 00:26:15 +0100

    @new 	added parameter patternT4htOutputFiles 	- had to endow method getPatternOutputFiles with parameter Settings, 	  which is used for target html only. 	- there, field HTML_OUTPUT_FILES was removed and replaced by parameter. 141-->146

--- maven-latex-plugin/src/main/java/org/m2latex/core/Target.java
+++ maven-latex-plugin/src/main/java/org/m2latex/core/Target.java
@@ -21,11 +21,13 @@ public enum Target {
      */
     rtf() {
 	private final String RTF_OUTPUT_FILES = "^T$T\\.rtf$";
+
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2rtf(texFile);
 	}
-	public String getPatternOutputFiles() {
+
+	public String getPatternOutputFiles(Settings settings) {
 	    return RTF_OUTPUT_FILES;
 	}
 
@@ -41,7 +43,8 @@ public enum Target {
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2pdf(texFile);
 	}
-	public String getPatternOutputFiles() {
+
+	public String getPatternOutputFiles(Settings settings) {
 	    return LATEX_OUTPUT_FILES;
 	}
     },
@@ -50,26 +53,13 @@ public enum Target {
      *
      */
     html() {
-	// FIXME: use class constants for suffixes 
-	// Documentation: 
-	// - xxx.(x)htm(l) is the main file. 
-	// - xxxlid.(x)htm(l) with digit(s) d 
-	//                    are lists: toc, lof, lot, indices, glossaries, 
-	//                    NOT the bibliography. 
-	// - xxxsed.(x)htm(l) with digit(s) d are sections. 
-	// - xxxsud.(x)htm(l) with digit(s) d are subsections or below. 
-	// - xxxd.(x)htm(l)   with digit(s) d are footnotes 
-	private final String HTML_OUTPUT_FILES = 
-	    "^(T$T(((se|su|li)?\\d+)?\\.x?html?|" + 
-	    "\\.css|" + 
-	    ".\\d+\\.svg)|" + 
-	    "(cmsy)\\d+(-c)?-\\d+c?\\.png)$";
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2html(texFile);
 	}
-	public String getPatternOutputFiles() {
-	    return HTML_OUTPUT_FILES;
+
+	public String getPatternOutputFiles(Settings settings) {
+	    return settings.getPatternT4htOutputFiles();
 	}
     },
     /**
@@ -79,12 +69,13 @@ public enum Target {
     odt() {
 	private final String OOFFICE_OUTPUT_FILES =
 	    "^T$T\\.(odt|fodt|uot|uot)$";
+
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2odt(texFile);
 	}
 
-	public String getPatternOutputFiles() {
+	public String getPatternOutputFiles(Settings settings) {
 	    return OOFFICE_OUTPUT_FILES;
 	}
     },
@@ -95,46 +86,60 @@ public enum Target {
     docx() {
 	private final String MSWORD_OUTPUT_FILES = 
 	    "^T$T\\.(doc(|6|.95|.x|.x7)|rtf)$";
+
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2docx(texFile);
 	}
-	public String getPatternOutputFiles() {
+
+	public String getPatternOutputFiles(Settings settings) {
 	    return MSWORD_OUTPUT_FILES;
 	}
-
     },
     /**
      * Based on {@link #pdf}
      */
     txt() {
 	private final String TXT_OUTPUT_FILES = "^T$T\\.txt$";
+
 	public void processSource(LatexProcessor latexProcessor, 
 				  File texFile) throws BuildExecutionException {
 	    latexProcessor.processLatex2txt(texFile);
 	}
-	public String getPatternOutputFiles() {
+
+	public String getPatternOutputFiles(Settings settings) {
 	    return TXT_OUTPUT_FILES;
 	}
     };
 
     /**
-     * Processes the source file <code>texFile</code> 
-     * according to the concrete Mojo. 
+     * Processes the latex main file <code>texFile</code> 
+     * delegating to <code>latexProcessor</code>. 
+     *
+     * @param latexProcessor
+     *    the processor to process <code>texFile</code> 
+     * @param texFile
+     *    the latex main file to be processed. 
      */
     public abstract void processSource(LatexProcessor latexProcessor, 
     				       File texFile) 
     throws BuildExecutionException;
 
-
     /**
-     * Returns the suffixes and wildcards of the output files. 
-     * For example if creating pdf and postscript, 
-     * this is just <code>.pdf, .ps</code> 
-     * but if various html files are created, it is <code>*.html</code>, 
-     * the asterisk representing a wildcard. 
+     * Returns the pattern of the output files. 
+     * For example if creating pdf, 
+     * this is just <code>^T$T\.pdf$</code>, 
+     * where <code>T$T</code> represents the name of the latex main file 
+     * without suffix. 
+     * For target {@link #html}, this is much more complicated, 
+     * because a lot of files are created in general, 
+     * not only <code>^T$T\.h?tml?$</code>. 
+     *
+     * @param settings
+     *    the settings required to determine the pattern. 
+     *    This depends on the settings for {@link #html} only. 
      */
-    public abstract String getPatternOutputFiles();
+    public abstract String getPatternOutputFiles(Settings settings);
 
 }
  
