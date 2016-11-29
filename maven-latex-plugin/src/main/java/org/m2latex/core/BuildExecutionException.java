cef3c4d7b7723784e8250ddcfd5f0b063c4741cc
Reissner <rei3ner@arcor.de>
2016-11-29 16:19:16 +0100

    @doc 	when to throw what.

--- maven-latex-plugin/src/main/java/org/m2latex/core/BuildExecutionException.java
+++ maven-latex-plugin/src/main/java/org/m2latex/core/BuildExecutionException.java
@@ -5,6 +5,18 @@ package org.m2latex.core;
  * to {@link org.apache.maven.plugin.MojoExecutionException} 
  * in {@link org.m2latex.mojo.AbstractLatexMojo#execute()} 
  * to avoid maven-specific classes. 
+ * <p>
+ * We read from the documentation which applies to this exception type also: 
+ * A MojoExecutionException is a fatal exception, 
+ * something unrecoverable happened. 
+ * You would throw a MojoExecutionException 
+ * if something happens that warrants a complete stop in a build; 
+ * you re trying to write to disk, but there is no space left, 
+ * or you were trying to publish to a remote repository, 
+ * but you can’t connect to it. 
+ * Throw a MojoExecutionException if there is no chance of a build continuing; 
+ * something terrible has happened 
+ * and you want the build to stop and the user to see a "BUILD ERROR" message.
  *
  * Created: Fri Sep 30 15:01:16 2016
  *
--- maven-latex-plugin/src/main/java/org/m2latex/core/BuildFailureException.java
+++ maven-latex-plugin/src/main/java/org/m2latex/core/BuildFailureException.java
@@ -5,6 +5,19 @@ package org.m2latex.core;
  * to {@link org.apache.maven.plugin.MojoFailureException} 
  * in {@link org.m2latex.mojo.AbstractLatexMojo#execute()} 
  * to avoid maven-specific classes. 
+ * <p>
+ * We read from the documentation which applies to this exception type also: 
+ * A MojoFailureException is something less catastrophic 
+ * (compared to a {@link org.apache.maven.plugin.MojoExecutionException}), 
+ * a goal can fail, but it might not be the end of the world 
+ * for your Maven build. 
+ * A unit test can fail, or a MD5 checksum can fail; 
+ * both of these are potential problems, 
+ * but you don’t want to return an exception 
+ * that is going to kill the entire build. 
+ * In this situation you would throw a MojoFailureException. 
+ * Maven provides for different "resiliency" settings 
+ * when it comes to project failure. 
  *
  * Created: Fri Sep 30 15:01:16 2016
  *
