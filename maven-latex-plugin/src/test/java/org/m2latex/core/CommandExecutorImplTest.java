626bcbc3e21d0a712ba99e60a908e44598c15e3a
Reissner <rei3ner@arcor.de>
2016-12-13 13:51:31 +0100

    @chg 	eliminated warnings: to that end, log files must exist. 	- replaced invocation of echo by invocation of touch in working dir.

--- maven-latex-plugin/src/test/java/org/m2latex/core/CommandExecutorImplTest.java
+++ maven-latex-plugin/src/test/java/org/m2latex/core/CommandExecutorImplTest.java
@@ -21,25 +21,65 @@ package org.m2latex.core;
 import org.m2latex.mojo.MavenLogWrapper;
 
 import java.io.File;
+import java.io.IOException;
 
 import junit.framework.TestCase;
 
 import org.apache.maven.plugin.logging.SystemStreamLog;
 
+import org.easymock.MockControl;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
 
 public class CommandExecutorImplTest {
+    private final static String WORKING_DIR = 
+	System.getProperty("testResourcesDir");
+
+    private MockControl executorCtrl = MockControl
+	.createStrictControl(CommandExecutor.class);
+
+    private CommandExecutor executor = (CommandExecutor) executorCtrl.getMock();
+
+    private static void cleanWorkingDir() {
+	File wDir = new File(WORKING_DIR);
+	assert wDir.isDirectory() : "Expected directory. ";
+	File[] files = wDir.listFiles();
+	assert files != null : "Working directory is not readable. ";
+	for (File file : files) {
+	    file.delete();
+	}
+    }
+
+    @Before public void setUp() throws IOException {
+	cleanWorkingDir();
+    }
+
     @Test public void testExecute() throws BuildFailureException {
         CommandExecutorImpl executor = 
 	    new CommandExecutorImpl(new MavenLogWrapper(new SystemStreamLog()));
-        String echoText = "LaTeX";
-        String output = executor.execute(new File("."), 
+ 	File workingDir = new File(WORKING_DIR);
+        String touchFile = "cmdLineExe.touch";
+	String output = executor.execute(workingDir, 
 					 null, 
-					 "echo", 
-					 new String[] {echoText},
-					 new File("."));
-        assertEquals( echoText, output.subSequence( 0, echoText.length() ) );
+					 "touch", 
+					 new String[] {touchFile},
+					 new File(workingDir, touchFile));
+        //assertEquals(echoText, output.subSequence(0, echoText.length()));
     }
+
+    // void mockExecute() throws BuildFailureException {
+    // 	File res = new File("/tmp/exists.latexPlugin");
+    // 	res.delete();
+    //     this.executor.execute(new File("/tmp/"),
+    // 			      null,
+    // 			      "touch",
+    // 			      new String[] {res.getName()},
+    // 			      res);
+    //     executorCtrl.setMatcher(MockControl.ARRAY_MATCHER);
+    //     executorCtrl.setReturnValue(null);
+    // }
 }
