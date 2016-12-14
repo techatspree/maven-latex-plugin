db1f852061307221f70f4a9a545b6ade02a07393
Reissner <rei3ner@arcor.de>
2016-12-14 15:13:09 +0100

    @chg 	unified WORKING_DIR with LatexProcessorTest 	so no unknown directory is used.

--- maven-latex-plugin/src/test/java/org/m2latex/core/TexFileUtilsTest.java
+++ maven-latex-plugin/src/test/java/org/m2latex/core/TexFileUtilsTest.java
@@ -20,29 +20,58 @@ package org.m2latex.core;
 
 import org.m2latex.mojo.MavenLogWrapper;
 
+import org.m2latex.core.BuildExecutionException;
+import org.m2latex.core.BuildFailureException;
+
 import org.apache.maven.plugin.logging.SystemStreamLog;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.m2latex.core.BuildExecutionException;
-import org.m2latex.core.BuildFailureException;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.After;
 
 public class TexFileUtilsTest {
+    private final static File WORKING_DIR = 
+	new File(System.getProperty("testResourcesDir"));
 
-    private TexFileUtils utils = 
-	new TexFileUtils(new MavenLogWrapper(new SystemStreamLog()));
+    // FIXME: occurs also in other testclasses: 
+    // to be unified. 
+    private static void cleanWorkingDir() {
+	assert WORKING_DIR.isDirectory() : "Expected directory. ";
+	File[] files = WORKING_DIR.listFiles();
+	assert files != null : "Working directory is not readable. ";
+	for (File file : files) {
+	    if (!file.isHidden()) {
+		file.delete();
+	    }
+	}
+    }
 
+    @Before public void setUp() throws IOException {
+	cleanWorkingDir();
+    }
+
+    @After public void tearDown() throws IOException {
+	cleanWorkingDir();
+    }
+
+
+ 
     @Test public void testGetTargetDir() throws BuildFailureException {
 
-        File expected = new File("/tmp/dir2/subdir");
+        File expected = new File(WORKING_DIR, "dir2/subdir");
+	TexFileUtils utils = 
+	    new TexFileUtils(new MavenLogWrapper(new SystemStreamLog()));
 	// may throw BuildFailureException 
         File actual = utils
-	    .getTargetDirectory(new File("/tmp/dir1/subdir/file"),
-				new File("/tmp/dir1"),
-				new File("/tmp/dir2"));
+	    .getTargetDirectory(new File(WORKING_DIR, "dir1/subdir/file"),
+				new File(WORKING_DIR, "dir1"),
+				new File(WORKING_DIR, "dir2"));
         assertEquals(expected, actual);
     }
 }
