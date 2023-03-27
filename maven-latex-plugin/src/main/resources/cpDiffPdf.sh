a24311f09c17c9d1515e018ab3ae0d6782f1051a
Ernst Reissner <ernst.reissner@simuline.eu>
2023-03-27 08:38:09 +0200

    Update cpDiffPdf.sh: added further artifacts.

--- maven-latex-plugin/src/main/resources/cpDiffPdf.sh
+++ maven-latex-plugin/src/main/resources/cpDiffPdf.sh
@@ -4,5 +4,8 @@
 pwd
 cp src/test/resources/integration/target/./manualLatexMavenPlugin.pdf     src/main/resources/docsCmp/manualLatexMavenPlugin.pdf 
 cp src/test/resources/integration/target/./dvi/dviFormat.pdf              src/main/resources/docsCmp/dvi/dviFormat.pdf
+cp src/test/resources/integration/target/./latex/latexEngines.pdf         src/main/resources/docsCmp/latex/latexEngines.pdf
+cp src/test/resources/integration/target/./pythontex/pythontexInOut.pdf   src/main/resources/docsCmp/pythontex/pythontexInOut.pdf
+cp src/test/resources/integration/target/./recorderOption/recorder.pdf    src/main/resources/docsCmp/recorderOption/recorder.pdf
 cp src/test/resources/integration/target/./robustIdxGlos/robustsample.pdf src/main/resources/docsCmp/robustIdxGlos/robustsample.pdf 
 cp src/test/resources/integration/target/./xfig/xfigFormat.pdf            src/main/resources/docsCmp/xfig/xfigFormat.pdf
