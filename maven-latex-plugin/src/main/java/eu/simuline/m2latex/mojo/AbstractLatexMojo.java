90c1a8736370d9b17bb670c36e25ab052d75bfc7
Ernst Reissner <ernst.reissner@simuline.eu>
2023-06-21 04:31:03 +0200

    formatting: AbstractLatexMojo.java and CfgLatexMojo.java

--- maven-latex-plugin/src/main/java/eu/simuline/m2latex/mojo/AbstractLatexMojo.java
+++ maven-latex-plugin/src/main/java/eu/simuline/m2latex/mojo/AbstractLatexMojo.java
@@ -26,115 +26,111 @@ import org.apache.maven.plugin.AbstractMojo;
 
 import org.apache.maven.plugins.annotations.Parameter;
 
-// TBD: use new dependency on slf4j 
-//import com.jcabi.log.Logger;
-//import org.slf4j.impl.StaticLoggerBinder;
+// TBD: use new dependency on slf4j
+// import com.jcabi.log.Logger;
+// import org.slf4j.impl.StaticLoggerBinder;
 
 import java.io.File;
 
 /**
- * Abstract base class for all mojos. 
+ * Abstract base class for all mojos.
  *
  */
-abstract class AbstractLatexMojo extends AbstractMojo 
+abstract class AbstractLatexMojo extends AbstractMojo
     implements ParameterAdapter {
 
-    /**
-     * The base directory of this maven project. 
-     * Reinitializes {@link Settings#baseDirectory} via {@link #initialize()}. 
-     */
-    @Parameter(name = "baseDirectory", 
-	       defaultValue = "${basedir}", 
-	       readonly = true)
-    protected File baseDirectory;
+  /**
+   * The base directory of this maven project. 
+   * Reinitializes {@link Settings#baseDirectory} via {@link #initialize()}. 
+   */
+  @Parameter(name = "baseDirectory", defaultValue = "${basedir}",
+      readonly = true)
+  protected File baseDirectory;
 
-    /**
-     * The target directory of this maven project. 
-     * Reinitializes {@link Settings#targetDirectory} 
-     * via {@link #initialize()}. 
-     */
-    @Parameter(name = "targetDirectory", 
-	       defaultValue = "${project.build.directory}", 
-	       readonly = true)
-    protected File targetDirectory;
+  /**
+   * The target directory of this maven project. 
+   * Reinitializes {@link Settings#targetDirectory} 
+   * via {@link #initialize()}. 
+   */
+  @Parameter(name = "targetDirectory",
+      defaultValue = "${project.build.directory}", readonly = true)
+  protected File targetDirectory;
 
-    /**
-     * The target site directory of this maven project. 
-     * Reinitializes {@link Settings#baseDirectory} via {@link #initialize()}. 
-     */
-    @Parameter(name = "targetSiteDirectory", 
-	       defaultValue = "${project.reporting.outputDirectory}", 
-	       readonly = true)
-    protected File targetSiteDirectory;
+  /**
+   * The target site directory of this maven project. 
+   * Reinitializes {@link Settings#baseDirectory} via {@link #initialize()}. 
+   */
+  @Parameter(name = "targetSiteDirectory",
+      defaultValue = "${project.reporting.outputDirectory}", readonly = true)
+  protected File targetSiteDirectory;
 
 
-    /**
-     * Indicates whether the {@link VersionMojo} displays warnings only; 
-     * else also creates infos. 
-     * Infos refer to the version of this plugin, 
-     * but also on the versions of the converters found 
-     * and on the converters excluded. 
-     * Warnings are emitted e.g. if a version does not fit the expectations. 
-     * This defaults to <code>false</code> displaying also info. 
-     * The latter is appropriate for using in command line 
-     * <code>mvn latex:vrs</code>, whereas in builds by default 
-     * the pom overwrites this to have output only 
-     * in case something goes wrong. 
-     */
-    @Parameter(name = "versionsWarnOnly", defaultValue = "false")
-    private boolean versionsWarnOnly;
+  /**
+   * Indicates whether the {@link VersionMojo} displays warnings only; 
+   * else also creates infos. 
+   * Infos refer to the version of this plugin, 
+   * but also on the versions of the converters found 
+   * and on the converters excluded. 
+   * Warnings are emitted e.g. if a version does not fit the expectations. 
+   * This defaults to <code>false</code> displaying also info. 
+   * The latter is appropriate for using in command line 
+   * <code>mvn latex:vrs</code>, whereas in builds by default 
+   * the pom overwrites this to have output only 
+   * in case something goes wrong. 
+   */
+  @Parameter(name = "versionsWarnOnly", defaultValue = "false")
+  private boolean versionsWarnOnly;
 
-    /**
-     * Comprises all parameters for executing this maven plugin. 
-     * If not set in the pom prior to execution, 
-     * is set in {@link #initialize()}. 
-     */
-    // for help plugin this does not fit. 
-    // also not ideal that settings is all the same independent of target. 
-    // also for help plugin this is not detailed enough. 
-    @Parameter(name = "settings")
-    protected Settings settings;
+  /**
+   * Comprises all parameters for executing this maven plugin. 
+   * If not set in the pom prior to execution, 
+   * is set in {@link #initialize()}. 
+   */
+  // for help plugin this does not fit. 
+  // also not ideal that settings is all the same independent of target. 
+  // also for help plugin this is not detailed enough. 
+  @Parameter(name = "settings")
+  protected Settings settings;
 
-    // set by {@link #initialize()}. 
-    protected LatexProcessor latexProcessor;
+  // set by {@link #initialize()}. 
+  protected LatexProcessor latexProcessor;
 
 
-    /**
-     * Returns whether the {@link VersionMojo} displays warnings only; 
-     * else also creates infos. 
-     *
-     * @return
-     *    whether the {@link VersionMojo} displays warnings only; 
-     *    lse also creates infos. 
-     */
-    public boolean getVersionsWarnOnly() {
-	return this.versionsWarnOnly;
+  /**
+   * Returns whether the {@link VersionMojo} displays warnings only; 
+   * else also creates infos. 
+   *
+   * @return
+   *    whether the {@link VersionMojo} displays warnings only; 
+   *    lse also creates infos. 
+   */
+  public boolean getVersionsWarnOnly() {
+    return this.versionsWarnOnly;
+  }
+
+  /**
+   * Sets whether {@link VersionMojo} displays warnings only; 
+   * else also creates infos. 
+   *
+   * @param versionsWarnOnly
+   *    whether the {@link VersionMojo} shall display warnings only. 
+   */
+  public void setVersionsWarnOnly(boolean versionsWarnOnly) {
+    this.versionsWarnOnly = versionsWarnOnly;
+  }
+
+  // api-docs inherited from ParameterAdapter 
+  public final void initialize() {
+    if (this.settings == null) {
+      // Here, no configuration is defined in pom, 
+      // i.e. object is not created by Maven
+      this.settings = new Settings();
     }
+    this.settings.setBaseDirectory(this.baseDirectory);
+    this.settings.setTargetSiteDirectory(this.targetSiteDirectory);
+    this.settings.setTargetDirectory(this.targetDirectory);
 
-    /**
-     * Sets whether {@link VersionMojo} displays warnings only; 
-     * else also creates infos. 
-     *
-     * @param versionsWarnOnly
-     *    whether the {@link VersionMojo} shall display warnings only. 
-     */
-    public void setVersionsWarnOnly(boolean versionsWarnOnly) {
-	this.versionsWarnOnly = versionsWarnOnly;
-    }
-
-    // api-docs inherited from ParameterAdapter 
-    public final void initialize() {
-	if (this.settings == null) {
-	    // Here, no configuration is defined in pom, 
-	    // i.e. object is not created by Maven
-	    this.settings = new Settings();
-	}
-	this.settings.setBaseDirectory(this.baseDirectory);
-	this.settings.setTargetSiteDirectory(this.targetSiteDirectory);
-	this.settings.setTargetDirectory(this.targetDirectory);
-
-	this.latexProcessor = new LatexProcessor(this.settings,  
-						 new MavenLogWrapper(getLog()), 
-						 this);
-    }
+    this.latexProcessor =
+        new LatexProcessor(this.settings, new MavenLogWrapper(getLog()), this);
+  }
 }
