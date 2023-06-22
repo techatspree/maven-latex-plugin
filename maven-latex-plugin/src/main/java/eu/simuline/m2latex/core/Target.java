cf1c8430d9eb88696939f4a7c760b23e67f0dc07
Ernst Reissner <ernst.reissner@simuline.eu>
2023-06-22 13:13:04 +0200

    Cleanup in Target.java

--- maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/Target.java
+++ maven-latex-plugin/src/main/java/eu/simuline/m2latex/core/Target.java
@@ -1,7 +1,5 @@
 package eu.simuline.m2latex.core;
 
-//import java.io.File;
-
 /**
  * The enumeration of all supported (creational) targets.
  * These are targets where tex files are converted into a target format
@@ -19,179 +17,176 @@ package eu.simuline.m2latex.core;
  */
 public enum Target {
 
-    /**
-     * The only target without artifact
-     */
-    chk() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                  LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processCheck(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return Target.NO_OUTPUT_FILES;
-        }
-    },
-    /**
-     * standalone.
-     */
-    dvi() {
-        // FIXME: how does this fit with preprocessing??
-
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                  LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2dvi(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^(T$T\\.(dvi|xdv)|.+(\\.(ptx|eps|jpg|png)|\\d+\\.mps))$";
-        }
-    },
-    /**
-     * standalone.
-     */
-    pdf() {
-         // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                  LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2pdf(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^T$T\\.pdf$";
-        }
-
-        public boolean hasDiffTool() {
-            return true;
-        }
-    },
-    /**
-     * Based on {@link #pdf}
-     *
-     */
-    html() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                  LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2html(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return settings.getPatternT4htOutputFiles();
-        }
-    },
-    /**
-     * Based on {@link #pdf}
-     *
-     */
-    odt() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor, 
-                                LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2odt(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^T$T\\.(odt|fodt|uot|uot)$";
-        }
-    },
-    /**
-     * Based on {@link #odt}
-     *
-     */
-    docx() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2docx(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^T$T\\.(doc(|6|.95|.x|.x7)|rtf)$";
-        }
-    },
-    /**
-     * standalone
-     *
-     */
-    rtf() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                  LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2rtf(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^T$T\\.rtf$";
-        }
-    },
-    /**
-     * Based on {@link #pdf}
-     */
-    txt() {
-        // may throw BuildFailureException TEX01
-        public void processSource(LatexProcessor latexProcessor,
-                                LatexMainDesc desc) throws BuildFailureException {
-            latexProcessor.processLatex2txt(desc);
-        }
-
-        public String getPatternOutputFiles(Settings settings) {
-            return "^T$T\\.txt$";
-        }
-    };
-
-    /**
-     * Regular expression which does not fit any file. 
-     */
-    private final static String NO_OUTPUT_FILES = ".^";
-
-    /**
-     * Processes the latex main file <code>texFile</code>
-     * delegating to <code>latexProcessor</code>.
-     * Logging: FIXME: may be incomplete
-     * <ul>
-     * <li>EEX01, EEX02, EEX03, WEX04, WEX05 if running a command
-     * to transform <code>texFile</code> failed.
-     * </ul>
-     *
-     * @param latexProcessor
-     *    the processor to process <code>texFile</code>
-     * @param desc
-     *    the latex main file to be processed.
-     * @throws BuildFailureException
-     *    TEX01 if invocation of a command
-     *    to transform <code>texFile</code> failed.
-     */
-    public abstract void processSource(LatexProcessor latexProcessor,
-            LatexMainDesc desc) throws BuildFailureException;
-
-    /**
-     * Returns the pattern of the output files.
-     * For example if creating pdf,
-     * this is just <code>^T$T\.pdf$</code>,
-     * where <code>T$T</code> represents the name of the latex main file
-     * without suffix.
-     * For target {@link #html}, this is much more complicated,
-     * because a lot of files are created in general,
-     * not only <code>^T$T\.h?tml?$</code>.
-     *
-     * @param settings
-     *                 the settings required to determine the pattern.
-     *                 This depends on the settings for {@link #html} only.
-     */
-    public abstract String getPatternOutputFiles(Settings settings);
-
-    /**
-     * Returns whether this target as an associated visual diff tool. 
-     * Currently this is the case for {@link #pdf} only. 
-     * 
-     * @return
-     *    whether this target as an associated diff tool. 
-     *    Currently this is the case for {@link #pdf} only. 
-     */
-    public boolean hasDiffTool() {
-        return false;
+  /**
+   * The only target without artifact. 
+   */
+  chk() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processCheck(desc);
     }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return Target.NO_OUTPUT_FILES;
+    }
+  },
+  /**
+   * standalone.
+   */
+  dvi() {
+    // FIXME: how does this fit with preprocessing??
+
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2dvi(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^(T$T\\.(dvi|xdv)|.+(\\.(ptx|eps|jpg|png)|\\d+\\.mps))$";
+    }
+  },
+  /**
+   * standalone.
+   */
+  pdf() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2pdf(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^T$T\\.pdf$";
+    }
+
+    public boolean hasDiffTool() {
+      return true;
+    }
+  },
+  /**
+   * Based on {@link #pdf}
+   */
+  html() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2html(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return settings.getPatternT4htOutputFiles();
+    }
+  },
+  /**
+   * Based on {@link #pdf}
+   *
+   */
+  odt() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2odt(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^T$T\\.(odt|fodt|uot|uot)$";
+    }
+  },
+  /**
+   * Based on {@link #odt}
+   */
+  docx() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2docx(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^T$T\\.(doc(|6|.95|.x|.x7)|rtf)$";
+    }
+  },
+  /**
+   * standalone
+   */
+  rtf() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2rtf(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^T$T\\.rtf$";
+    }
+  },
+  /**
+   * Based on {@link #pdf}
+   */
+  txt() {
+    // may throw BuildFailureException TEX01
+    public void processSource(LatexProcessor latexProcessor, LatexMainDesc desc)
+        throws BuildFailureException {
+      latexProcessor.processLatex2txt(desc);
+    }
+
+    public String getPatternOutputFiles(Settings settings) {
+      return "^T$T\\.txt$";
+    }
+  };
+
+  /**
+   * Regular expression which does not fit any file. 
+   */
+  private final static String NO_OUTPUT_FILES = ".^";
+
+  /**
+   * Processes the latex main file <code>texFile</code>
+   * delegating to <code>latexProcessor</code>.
+   * Logging: FIXME: may be incomplete
+   * <ul>
+   * <li>EEX01, EEX02, EEX03, WEX04, WEX05 if running a command
+   * to transform <code>texFile</code> failed.
+   * </ul>
+   *
+   * @param latexProcessor
+   *    the processor to process <code>texFile</code>
+   * @param desc
+   *    the latex main file to be processed.
+   * @throws BuildFailureException
+   *    TEX01 if invocation of a command
+   *    to transform <code>texFile</code> failed.
+   */
+  public abstract void processSource(LatexProcessor latexProcessor,
+          LatexMainDesc desc) throws BuildFailureException;
+
+  /**
+   * Returns the pattern of the output files.
+   * For example if creating pdf,
+   * this is just <code>^T$T\.pdf$</code>,
+   * where <code>T$T</code> represents the name of the latex main file
+   * without suffix.
+   * For target {@link #html}, this is much more complicated,
+   * because a lot of files are created in general,
+   * not only <code>^T$T\.h?tml?$</code>.
+   *
+   * @param settings
+   *    the settings required to determine the pattern.
+   *    This depends on the settings for {@link #html} only.
+   */
+  public abstract String getPatternOutputFiles(Settings settings);
+
+  /**
+   * Returns whether this target as an associated visual diff tool. 
+   * Currently this is the case for {@link #pdf} only. 
+   * 
+   * @return
+   *    whether this target as an associated diff tool. 
+   *    Currently this is the case for {@link #pdf} only. 
+   */
+  public boolean hasDiffTool() {
+    return false;
+  }
 }
