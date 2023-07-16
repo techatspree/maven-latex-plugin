efb167decdc1b8980450f42278609d1b1705eb15
Ernst Reissner <ernst.reissner@simuline.eu>
2023-07-17 00:06:22 +0200

    Update VersionMojo.java: reformatting.

--- maven-latex-plugin/src/main/java/eu/simuline/m2latex/mojo/VersionMojo.java
+++ maven-latex-plugin/src/main/java/eu/simuline/m2latex/mojo/VersionMojo.java
@@ -34,34 +34,34 @@ import org.apache.maven.plugin.MojoFailureException;
 // in fact, Metainfo can give more info than just versioning 
 public class VersionMojo extends AbstractLatexMojo {
 
-     /**
-      * Prints meta information, mainly version information 
-      * on this software and on the converters used. 
-      * <p>
-      * WMI01: If the version string of a converter cannot be read. 
-      * WMI02: If the version of a converter is not as expected. 
-      *
-      * @throws MojoFailureException
-      *    <ul>
-      *    <li>TMI01: if the stream to either the manifest file 
-      *        or to a property file, either {@LINK #VERSION_PROPS_FILE} 
-      *        or {@link eu.simuline.m2latex.core.MetaInfo.GitProperties#GIT_PROPS_FILE} could not be created. </li>
-      *    <li>TMI02: if the properties could not be read 
-      *        from one of the two property files mentioned above. </li>
-      *    <li>TSS05: if converters are excluded in the pom which are not known. </li>
-      *    </ul>
-      */
-    public void execute() throws MojoFailureException {
-	// TBD: redesign 
-	initialize();
-	try {
-	    // warnings: WMI01, WMI02, 
-	    // may throw build failure exception TSS05
-	    this.latexProcessor.printMetaInfo(!this.getVersionsWarnOnly());
-	} catch (BuildFailureException e) {
-	    // may throw Exception TMI01, TMI02, TSS05
-	    throw new MojoFailureException(e.getMessage(), e.getCause());
-	}
+  /**
+   * Prints meta information, mainly version information 
+   * on this software and on the converters used. 
+   * <p>
+   * WMI01: If the version string of a converter cannot be read. 
+   * WMI02: If the version of a converter is not as expected. 
+   *
+   * @throws MojoFailureException
+   *    <ul>
+   *    <li>TMI01: if the stream to either the manifest file 
+   *        or to a property file, either {@LINK #VERSION_PROPS_FILE} 
+   *        or {@link eu.simuline.m2latex.core.MetaInfo.GitProperties#GIT_PROPS_FILE} could not be created. </li>
+   *    <li>TMI02: if the properties could not be read 
+   *        from one of the two property files mentioned above. </li>
+   *    <li>TSS05: if converters are excluded in the pom which are not known. </li>
+   *    </ul>
+   */
+  public void execute() throws MojoFailureException {
+    // TBD: redesign 
+    initialize();
+    try {
+      // warnings: WMI01, WMI02, 
+      // may throw build failure exception TSS05
+      this.latexProcessor.printMetaInfo(!this.getVersionsWarnOnly());
+    } catch (BuildFailureException e) {
+      // may throw Exception TMI01, TMI02, TSS05
+      throw new MojoFailureException(e.getMessage(), e.getCause());
     }
+  }
 
 }
