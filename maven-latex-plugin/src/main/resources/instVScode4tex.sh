89e0ef80868aa367efd0db7924d0e03ba050d9cd
Ernst Reissner <ernst.reissner@simuline.eu>
2023-09-18 23:32:05 +0200

    make instVScode4tex.sh usable for alternative of `code` affects also manualLMP.pdf

--- maven-latex-plugin/src/main/resources/instVScode4tex.sh
+++ maven-latex-plugin/src/main/resources/instVScode4tex.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -
 
+# USAGE: 
+# without argument defaults to `code`
+# with single argument giving the name of the clone of code. 
+# Currently we use `CODE` for the flatpack version. 
+# This is used by eu.simuline:qMngmnt:0.0.6
+
 # Explanation 
 # code --extensions-dir <dir>
 #     Set the root path for extensions.
@@ -15,34 +21,47 @@
 #     Enables proposed API features for extensions. 
 #     Can receive one or more extension IDs to enable individually.
 
+case $# in 
+  0)
+  code="code"
+  ;;
+  1)
+  code=$1
+  ;;
+  *)
+  echo "expected 0 or 1 arguments found $#."
+  exit
+esac
+echo $code
+
 echo "latex and friends"
 # latex and friends 
-code --force --install-extension james-yu.latex-workshop
-code --force --install-extension mathematic.vscode-latex
+$code --force --install-extension james-yu.latex-workshop
+$code --force --install-extension mathematic.vscode-latex
 # lua 
 # [lua]: Couldn't find message for key config.runtime. ...
 #code --force --install-extension sumneko.lua
 
 # bib
-code --force --install-extension phr0s.bib
-code --force --install-extension twday.bibmanager
+$code --force --install-extension phr0s.bib
+$code --force --install-extension twday.bibmanager
 # nothing found for tikz
 # metapost 
-code --force --install-extension fjebaker.vscode-metapost
+$code --force --install-extension fjebaker.vscode-metapost
 # gnuplot is separate below 
 # TBD: zotero
 
 # gnuplot
-code --force --install-extension marioschwalbe.gnuplot
-code --force --install-extension fizzybreezy.gnuplot
+$code --force --install-extension marioschwalbe.gnuplot
+$code --force --install-extension fizzybreezy.gnuplot
 
 # svg
-code --force --install-extension jock.svg
-code --force --install-extension simonsiefke.svg-preview
+$code --force --install-extension jock.svg
+$code --force --install-extension simonsiefke.svg-preview
 
 # spellchecker
 code --force --install-extension valentjn.vscode-ltex
 
 # perl (e.g. to configure latexmk)
-code --force --install-extension d9705996.perl-toolbox
+$code --force --install-extension d9705996.perl-toolbox
 
